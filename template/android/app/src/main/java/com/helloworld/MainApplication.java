dc3a8037ebafff7b0490879f0a25225948fba904
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2021-08-19 14:03:49 -0300

    fix: import libs

--- template/android/app/src/main/java/com/helloworld/MainApplication.java
+++ template/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.security.Security;
+import android.os.Build;
 
 public class MainApplication extends Application implements ReactApplication {
 
